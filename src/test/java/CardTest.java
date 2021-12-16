fa4d61c0b6c001f826cbe514399fe4ba38811db6
aa_maslov <smart.maslov@gmail.com>
2021-12-16 15:16:52 +0300

    исправление №3

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -28,7 +28,7 @@ class CardTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
